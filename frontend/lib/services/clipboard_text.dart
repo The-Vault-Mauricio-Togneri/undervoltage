d2c224dc13b5cca98584169a245df687f025e359
Mauricio Togneri <mauricio.togneri@gmail.com>
2023-10-04 22:36:21 +0200

    Improvements

--- frontend/lib/services/clipboard_text.dart
+++ frontend/lib/services/clipboard_text.dart
@@ -1,4 +1,5 @@
-import 'package:undervoltage/platform/platform_mobile.dart' if (dart.library.html) 'package:undervoltage/platform/platform_web.dart';
+import 'package:undervoltage/platform/platform_mobile.dart'
+    if (dart.library.html) 'package:undervoltage/platform/platform_web.dart';
 
 class ClipboardText {
   void copy(String text) => PlatformMethods().clipboard(text);
--- website/functions/src/index.ts
+++ website/functions/src/index.ts
@@ -1,19 +1,33 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as admin from 'firebase-admin';
+import {CallableRequest, onCall} from 'firebase-functions/v2/https';
+import {onCreateMatch} from './entry-points/on-create-match';
+import {onValueUpdated} from 'firebase-functions/v2/database';
+import {onMatchUpdated} from './entry-points/on-match-updated';
+import {onJoinMatch} from './entry-points/on-join-match';
+import {UserRecord} from 'firebase-admin/auth';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const isEmulator = Boolean(process.env.FUNCTIONS_EMULATOR);
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+type CallableHandler = (request: CallableRequest, user: UserRecord) => Promise<unknown>
+
+const handleCallable = (handler: CallableHandler) => {
+  return onCall(async (request: CallableRequest) => {
+    const user = await admin.auth().getUser(request.auth?.uid ?? '');
+    const result = await handler(request, user);
+
+    return result;
+  });
+};
+
+export const createMatch = handleCallable(onCreateMatch);
+
+export const joinMatch = handleCallable(onJoinMatch);
+
+export const onMatchUpdatedTrigger = onValueUpdated({
+  ref: '/matches/{matchId}',
+  region: isEmulator ? 'us-central1' : 'europe-west1',
+}, (event) => {
+  return onMatchUpdated(event.data.after, event.params);
+});
