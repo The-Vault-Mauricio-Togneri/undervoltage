bfec4af76ae6533a8eb98420e2d4abcb636cd0b1
Mauricio Togneri <mauricio.togneri@gmail.com>
2023-11-04 16:36:33 +0100

    Improvements

--- frontend/lib/widgets/base_screen.dart
+++ frontend/lib/widgets/base_screen.dart
@@ -1,6 +1,5 @@
 import 'package:dafluta/dafluta.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 class BaseScreen extends StatelessWidget {
   final Widget child;
@@ -10,20 +9,18 @@ class BaseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DarkStatusBar(
-      child: KeyboardDismissOnTap(
-        child: Scaffold(
-          body: SafeArea(
-            child: CustomScrollView(
-              slivers: [
-                SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [Expanded(child: child)],
-                  ),
+      child: Scaffold(
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [Expanded(child: child)],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
